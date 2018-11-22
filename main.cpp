cb8cfd66de358fec8668a1bef42e9c0cea3ea1ea
Omar Alhammadi <sequencek@gmail.com>
2018-11-22 21:18:51 +0400

    added OpenMP support

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <SDL2/SDL.h>
+#include <iostream>
+#include <omp.h>
 
-int main(int argc, char *argv[]) {
+using namespace std;
+
+int SDL_main(int argc, char **argv) {
 
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    while (1) {
+    //while (1) {
+#pragma omp parallel for
+	for(int i=0;i<10;i++)
+		cout << omp_get_thread_num() << endl;
 
-    }
+    //}
     SDL_Quit();
     return 0;
 }
